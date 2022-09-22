1ffac4ea15b2c63236b56b1a2bf23aaf71ee1c37
Aditya Raj <raj.aditya@gmail.com>
2022-09-22 18:24:40 +0530

    Added various packages and TypeScript support

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
-var express = require('express');
-var { graphqlHTTP } = require('express-graphql');
-var { buildSchema } = require('graphql');
+console.log("hello");
 
 //https://www.section.io/engineering-education/build-a-graphql-server-using-nodejs/
 //https://blog.logrocket.com/creating-scalable-graphql-api-mysql-apollo-node/
 //https://www.digitalocean.com/community/tutorials/how-to-use-postgresql-with-node-js-on-ubuntu-20-04
-
+//https://www.freecodecamp.org/news/how-to-use-typescript-with-graphql/
 // Construct a schema, using GraphQL schema language
+
+/*
+
+var express = require('express');
+var { graphqlHTTP } = require('express-graphql');
+var { buildSchema } = require('graphql');
+
 var schema = buildSchema(`
   type Query {
     hello: String
@@ -28,3 +33,4 @@ app.use('/graphql', graphqlHTTP({
 }));
 app.listen(4000);
 console.log('Running a GraphQL API server at http://localhost:4000/graphql');
+*/
